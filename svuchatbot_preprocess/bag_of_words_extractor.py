8b94991e6f10cbfb3a2194b963f8bf16e61b9666
AnasJs <anas.jamal.mouhamad@gmail.com>
2022-08-01 22:45:10 +0300

    enhancement the tokenizer and bag of words extractor

--- svuchatbot_preprocess/bag_of_words_extractor.py
+++ svuchatbot_preprocess/bag_of_words_extractor.py
@@ -8,21 +8,41 @@ from collections import Counter
 from copy import copy, deepcopy
 from pprint import pprint
 
+
 class BagOfWordsExtractor(Extractor):
-    def __init__(self, source, field_name, n_cores):
+    def __init__(self, source, field_name, n_cores,  target=None, n_gram=1):
         super().__init__(source, field_name, n_cores)
         self.bag = Manager().list()
         self.bag_dict = Manager().dict()
+        self.n_gram = n_gram
+        if target is None:
+            self.t_db_name = source[0]
+            self.t_col_name = "bag_of_words"
+        else:
+            self.t_db_name= target[0]
+            self.t_col_name = target[1]
+
+    def __inject_words(self, item_id, words):
+        self.bag += words
+        self.bag_dict[item_id] = Counter(words)
 
     def _do(self, ids):
         col = get_collection(self.db_name, self.col_name)
-        cursor = col.find({"_id": {"$in": ids}}, ["_id",self.field_name])
-        for item in cursor:
-            self.bag += item[self.field_name]
-            self.bag_dict[item["_id"]] = Counter(item[self.field_name])
+        cursor = col.find({"_id": {"$in": ids}}, ["_id", self.field_name])
+        if self.n_gram == 1:
+            for item in cursor:
+                words = item[self.field_name]
+                # self.bag += words
+                # self.bag_dict[item["_id"]] = Counter(words)
+                self.__inject_words(item["_id"], words)
+        elif self.n_gram > 1:
+            for item in cursor:
+                words = [" ".join([item[self.field_name][j] for j in range(i, i+self.n_gram)])
+                         for i in range(len(item[self.field_name])-self.n_gram)]
+                self.__inject_words(item["_id"], words)
 
     def _do_after(self, ids):
-        col = get_collection(self.db_name, "bag_of_words")
+        col = get_collection(self.t_db_name, self.t_col_name)
         bag_dict = deepcopy(self.bag_dict)
         bag = deepcopy(self.bag)
         for item_id in ids:
@@ -34,9 +54,6 @@ class BagOfWordsExtractor(Extractor):
 
     def work(self, do=None):
         super().work()
+        # for _, v in self.bag_dict.items():
+        #     pprint(v)
         super().work(do=self._do_after)
-
-
-
-
-
--- svuchatbot_preprocess/tokens_extractor.py
+++ svuchatbot_preprocess/tokens_extractor.py
@@ -6,6 +6,13 @@ from svuchatbot_preprocess.extractor import Extractor
 
 
 class TokensExtractor(Extractor):
+    def __init__(self, source, field_name, n_cores, target= None):
+        super().__init__(source, field_name, n_cores)
+        if target is None:
+            self.t_col_name = "tokens"
+        else:
+            self.t_col_name = target[1]
+
     @staticmethod
     def nltk_based_tokenize_for_sentence(sent):
         res = []
@@ -32,5 +39,5 @@ class TokensExtractor(Extractor):
         col = get_collection(self.db_name, self.col_name)
         cursor = col.find({"_id": {"$in": ids}})
         for item in cursor:
-            item.update({"tokens": TokensExtractor.camle_based_tokenize_for_sentence(item[self.field_name])})
+            item.update({self.t_col_name: TokensExtractor.camle_based_tokenize_for_sentence(item[self.field_name])})
             cursor.collection.replace_one({"_id": item["_id"]}, item)
