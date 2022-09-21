947eb27fb06fad1010f37ea22023e4bbe18335ac
AnasJs <anas.jamal.mouhamad@gmail.com>
2022-09-21 11:02:28 +0300

    move csv files

--- src/svuchatbot_helper/read_specializations.py
+++ src/svuchatbot_helper/read_specializations.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def get_specializations():
-    f_path = join(get_project_root(), 'data', "Specializations_of_the_Syrian_Virtual_University.csv")
+    f_path = join(get_project_root(), 'assets', "Specializations_of_the_Syrian_Virtual_University.csv")
     df = pd.read_csv(f_path, names=["name0", "name1", "name2", "name3"])
     df = df.applymap(str.strip)
     return df
